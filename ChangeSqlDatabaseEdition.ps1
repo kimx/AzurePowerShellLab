7b50e9f6d708e6fa3e6c2e56e09770d782a1036a
kimx <kimxinfo@gmail.com>
2015-10-21 14:42:58 +0800

    init

---
+++ ChangeSqlDatabaseEdition.ps1
@@ -0,0 +1,8 @@
+﻿
+$serverContext="mysql"
+#1.取得資料庫
+$db = Get-AzureSqlDatabase $serverContext –DatabaseName "myDb"
+#2.取得服務層級物件
+$level=Get-AzureSqlDatabaseServiceObjective  $serverContext -ServiceObjectiveName "P1" 
+#3.調整資料庫層級
+Set-AzureSqlDatabase $serverContext –Database $db –ServiceObjective $level -Force –Edition Premium 
---
+++ Login.ps1
@@ -0,0 +1,11 @@
+﻿#1.取得發行檔
+Get-AzurePublishSettingsFile
+
+#2.匯入發行檔
+Import-AzurePublishSettingsFile -PublishSettingsFile "E:\GitHub\AzureSqlLevelChangeLab\Free Trial-10-21-2015-credentials.publishsettings"
+
+#3.切換訂閱
+Select-AzureSubscription -SubscriptionId "xxxx-8b63-4874-a1e2-xxxxxx"
+
+
+#ps:以上請分批執行
